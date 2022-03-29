b9dcb7268416e40379ce4691797b5ea3d7d7707c
vrossi96 <vrossi396@yahoo.it>
2022-03-29 23:22:14 +0200

    add faker users in user seeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Seeder;
 use App\User;
 
+use Faker\Generator as Faker;
+
 class UserSeeder extends Seeder
 {
     /**
@@ -10,7 +12,7 @@ class UserSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
         $user = new User();
         $user->name = 'admin';
@@ -18,5 +20,14 @@ class UserSeeder extends Seeder
         $user->password = bcrypt('admin');
 
         $user->save();
+
+        for ($i = 0; $i < 5; $i++) {
+            $auto_user = new User();
+            $auto_user->name = $faker->word();
+            $auto_user->email = $faker->email();
+            $auto_user->password = bcrypt('admin');
+
+            $auto_user->save();
+        }
     }
 }
