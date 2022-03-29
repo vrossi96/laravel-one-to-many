b0753e9c55a5b5bf44746fce8bae736bb19e4472
vrossi96 <vrossi396@yahoo.it>
2022-03-29 19:07:52 +0200

    add category select for edit post

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Post;
+use App\Models\Category;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
@@ -70,7 +71,8 @@ class PostController extends Controller
      */
     public function edit(Post $post)
     {
-        return view('admin.posts.edit', compact('post'));
+        $categories = Category::all();
+        return view('admin.posts.edit', compact('post', 'categories'));
     }
 
     /**
--- resources/views/admin/posts/includes/form.blade.php
+++ resources/views/admin/posts/includes/form.blade.php
@@ -1,21 +1,36 @@
 @if ($post->exists)
-   <form action="{{ route('admin.posts.update', $post->id) }}" method="POST">
+   <form class="row" action="{{ route('admin.posts.update', $post->id) }}" method="POST">
       @method('PUT')
    @else
-      <form action="{{ route('admin.posts.store') }}" method="POST">
+      <form class="row" action="{{ route('admin.posts.store') }}" method="POST">
 @endif
 @csrf
-<div class="form-group">
+{{-- FORM NAME POST --}}
+<div class="form-group col-md-10">
    <label for="title">Title</label>
    <input name="title" type="text" class="form-control" id="title" placeholder="Enter title"
       value="{{ old('title', $post->title) }}">
    <small class="form-text text-muted">Post title</small>
 </div>
-<div class="form-group">
+{{-- FORM CATEGORY --}}
+<div class="form-group col-md-2">
+   <label for="category">Category</label>
+   <select name="category_id" id="category">
+      <option value="null">Select a category</option>
+      @foreach ($categories as $category)
+         <option @if (old('category_id', $post->category_id) == $category->id) selected @endif value="{{ $category->id }}">
+            {{ $category->name }}
+         </option>
+      @endforeach
+   </select>
+</div>
+{{-- FORM CONTENT --}}
+<div class="form-group col-md-12">
    <label for="content">Content</label>
    <textarea class="w-100" name="content" id="content" rows="9">{{ old('content', $post->content) }}</textarea>
 </div>
-<div class="form-group">
+{{-- FORM IMAGE --}}
+<div class="form-group col-md-12">
    <label for="img">Image</label>
    <input class="w-100" type="text" name="img" id="img" value="{{ old('img', $post->img) }}">
 </div>
@@ -23,5 +38,7 @@
       <input type="checkbox" class="form-check-input" id="exampleCheck1">
       <label class="form-check-label" for="exampleCheck1">Check me out</label>
    </div> --}}
-<button type="submit" class="btn btn-primary"><i class="fa-solid fa-check"></i></button>
+<div class="col-md-12">
+   <button type="submit" class="btn btn-primary"><i class="fa-solid fa-check"></i></button>
+</div>
 </form>
